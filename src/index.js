c5193e87507cc0118bc2e915ac5893d0629ff07f
Rodolfo Montecino <vsuazo.93@gmail.com>
2021-02-08 09:44:14 -0300

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import cipher from './cipher.js';
 
+//Guardando datos ingresados
+
+const ejecCIfrar = document.getElementById("cifrar");
+const ejecDescifrar = document.getElementById("descifrar");
+
+ejecCIfrar.onclick = function(){
+    const disPlace = document.getElementById("desplazamiento").value;
+    const teXt1 = document.getElementById("original").value;
+    
+    console.log(disPlace);
+    console.log(teXt1);
+}
+
+ejecDescifrar.onclick = function(){
+    const disPlace = document.getElementById("desplazamiento").value;
+    const teXt1 = document.getElementById("original").value;
+    
+    console.log(disPlace);
+    console.log(teXt1);    
+}
+
+
 console.log(cipher);
