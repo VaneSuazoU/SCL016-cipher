b7b325d071d237ce2a3854427692c1909388a3f8
Rodolfo Montecino <vsuazo.93@gmail.com>
2021-02-16 09:32:39 -0300

    encode y decode listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,33 @@
 const cipher = {
 
-    encode: (teXt1, disPlace) => {
-        let msjListo = '';
+  encode: (teXt1, disPlace) => {
+      let msjListo = '';
+  for (let i = 0; i < teXt1.length; i++)
+  if (teXt1.charCodeAt(i) >= 65 && teXt1.charCodeAt(i) <= 90) {
+      let cipher = (teXt1.charCodeAt(i) - 65 + parseInt(disPlace)) % 26 + 65;
+      msjListo += String.fromCharCode(cipher);
+    } else {
+      msjListo += teXt1[i];
+    }
+    
+    return msjListo;
+
+  },
+
+decode: (teXt1, disPlace) => {
+    let msjListo = '';
     for (let i = 0; i < teXt1.length; i++)
-    // Si  usuario ingresa letras en minuscula
-    if (teXt1.charCodeAt(i) >= 97 && teXt1.charCodeAt(i) <= 122) {
-        let cipher = (teXt1.charCodeAt(i) - 97 + parseInt(disPlace)) % 26 + 97;
+      if (teXt1.charCodeAt(i) >= 65 && teXt1.charCodeAt(i) <= 90) {
+        let cipher = (teXt1.charCodeAt(i) + 65 - parseInt(disPlace)) % 26 + 65;
         msjListo += String.fromCharCode(cipher);
-        // Si usuario ingresa mayúsculas
-      } else if (teXt1.charCodeAt(i) >= 65 && teXt1.charCodeAt(i) <= 90) {
-        let cipher = (teXt1.charCodeAt(i) - 65 + parseInt(offsetCode)) % 26 + 65;
-        msjListo += String.fromCharCode(cipher);
-        // En caso de espacios y carácteres especiales
       } else {
         msjListo += teXt1[i];
-      };
-      return msjListo;
+      }
 
-    }
- }
- 
- 
- export default cipher;
-  
+    return msjListo;
+
+  }
+
+}
+
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import cipher from './cipher.js';
 const ejecCIfrar = document.getElementById("cifrar");
 const ejecDescifrar = document.getElementById("descifrar");
 
+// Pasando letras a mayuscula
+
+document.getElementById("original").addEventListener("keyup", myFunction);
+
+function myFunction() {
+  const x = document.getElementById("original");
+  x.value = x.value.toUpperCase();
+}
+
 ejecCIfrar.onclick = () => {
    const disPlace = document.getElementById("desplazamiento").value;
    const teXt1 = document.getElementById("original") .value;
@@ -22,6 +31,8 @@ ejecDescifrar.onclick = () => {
     const disPlace = document.getElementById("desplazamiento").value;
     const teXt1 = document.getElementById("original").value;
     document.getElementById("resultado").value = teXt1;
+    const msjeOk = cipher.decode(teXt1, disPlace);
+    document.getElementById ("resultado").value = msjeOk;
 
     console.log(document.getElementById("resultado"));
     console.log(disPlace);
